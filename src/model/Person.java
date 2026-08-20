a931bcc87637c6fde65807dceb2c855f6d2739e6
isuru-lakmal <30365503+ilpeiris@users.noreply.github.com>
2026-08-20 19:23:52 +0530

    Create Person.java

---
+++ src/model/Person.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ilpeiris
+ */
+public class Person {
+    private String name;
+    private String address;
+    private String contactNumber;
+    
+    public Person() {}
+    
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.name = name; }
+    
+    public String getContactNumber() { return contactNumber; }
+    public void setContactNumber(String contactNumber) { this.contactNumber = contactNumber; }
+    
+}
